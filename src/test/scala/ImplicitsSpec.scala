9f964e7c8717c836729b2bb0f75f233609f04d4f
Loïc Descotte <loic.descotte@kelkoo.com>
2016-04-18 16:04:36 +0200

    right biased either test

--- src/test/scala/ImplicitsSpec.scala
+++ src/test/scala/ImplicitsSpec.scala
@@ -3,7 +3,25 @@ import io.github.hamsters.Implicits
 
 class ImplicitsSpec extends FlatSpec with Matchers{
 
-  "Validation" should "give failures and successes" in {
+  "Either" should "compose using flatMap and map" in {
+
+    import Implicits._
+
+    val e1: Either[String, Int] = Right(1)
+    val e2: Either[String, Int] = Right(2)
+    val e3: Either[String, Int] = Right(3)
+
+    val combine = for {
+      v1 <- e1
+      v2 <- e2
+      v3 <- e3
+    } yield(s"$v1-$v2-$v3")
+
+    combine should be(Right("1-2-3"))
+
+  }
+
+  "Either" should "stop at first error" in {
 
     import Implicits._
 
