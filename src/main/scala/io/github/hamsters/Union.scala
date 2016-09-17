c42be243bfbc45ab094e387e2afc42e19fae0596
loicdescotte <loic.descotte@gmail.com>
2016-09-17 14:04:45 +0200

    removed useless implicit parameters

--- src/main/scala/io/github/hamsters/Union.scala
+++ src/main/scala/io/github/hamsters/Union.scala
@@ -40,9 +40,9 @@ case class Union4[T1, T2, T3, T4](v1: Option[T1], v2: Option[T2], v3: Option[T3]
 case class Union5[T1, T2, T3, T4, T5](v1: Option[T1], v2: Option[T2], v3: Option[T3], v4: Option[T4], v5: Option[T5])
 
 class Union2Type[T1, T2] {
-  implicit def toUnion21(t: T1)(implicit m1: Manifest[T1], m2: Manifest[T2]) = new Union2[T1, T2](Some(t), None)
+  implicit def toUnion21(t: T1) = new Union2[T1, T2](Some(t), None)
 
-  implicit def toUnion22(t: T2)(implicit m1: Manifest[T1], m2: Manifest[T2]) = new Union2[T1, T2](None, Some(t))
+  implicit def toUnion22(t: T2) = new Union2[T1, T2](None, Some(t))
 }
 
 class Union3Type[T1, T2, T3] {
