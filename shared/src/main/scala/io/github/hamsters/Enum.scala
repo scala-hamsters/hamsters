1aa2244ae20832065066dcef2817a3b6c602534f
Loïc Descotte <loic.descotte@kelkoo.com>
2017-08-23 12:00:43 +0200

    scaladoc

--- shared/src/main/scala/io/github/hamsters/Enum.scala
+++ shared/src/main/scala/io/github/hamsters/Enum.scala
@@ -11,9 +11,29 @@ trait Enumerable[A] {
 
 object Enumeration {
 
+  /**
+   * String name for an Enumerable object
+   * @param a
+   * @param ev
+   * @tparam A
+   * @return name
+   */
   def name[A](a: A)(implicit ev: Enumerable[_ >: A]): String = ev.name(a)
 
+  /**
+   * Resolve Option of Enumerable object from String
+   * @param s
+   * @param ev
+   * @tparam A
+   * @return Enumerable object
+   */
   def parse[A](s: String)(implicit ev: Enumerable[_ <: A]): Option[A] = ev.parse(s)
 
+  /**
+   * List Enumerable objects for an Enumerable type
+   * @param ev
+   * @tparam A
+   * @return List of Enumerable objects
+   */
   def list[A](implicit ev: Enumerable[A]): List[A] = ev.list
 }
