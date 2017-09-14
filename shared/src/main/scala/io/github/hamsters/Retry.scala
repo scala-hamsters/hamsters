6e0aee64a18721332895743f227de7aa57f76ca3
Loïc Descotte <loic.descotte@kelkoo.com>
2017-09-14 17:03:04 +0200

    useless package name

--- shared/src/main/scala/io/github/hamsters/Retry.scala
+++ shared/src/main/scala/io/github/hamsters/Retry.scala
@@ -12,9 +12,9 @@ object Retry {
    * @return Try of result
    */
   @annotation.tailrec
-  def retry[T](n: Int)(fn: => T): util.Try[T] = {
+  def retry[T](n: Int)(fn: => T): Try[T] = {
     // Borrowed from https://stackoverflow.com/questions/7930814/whats-the-scala-way-to-implement-a-retry-able-call-like-this-one
-    util.Try { fn } match {
+    Try { fn } match {
       case x: util.Success[T] => x
       case _ if n > 1 => retry(n - 1)(fn)
       case fn => fn
