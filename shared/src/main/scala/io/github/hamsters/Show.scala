dbf7f4d3fd2cb40809c4d0d7f8241fed32b55abc
dgouyette <damien.gouyette@gmail.com>
2017-10-14 08:40:49 +0200

    first version of Show

--- shared/src/main/scala/io/github/hamsters/Show.scala
+++ shared/src/main/scala/io/github/hamsters/Show.scala
@@ -1,37 +1,33 @@
 package io.github.hamsters
 
-//TODO remove this case classes, just here for the draft
-case class Name(firstName: String, lastName: String)
-case class Person(name: Name, age: Int)
-
 trait Showable[A] {
   def format(value: A): String
 }
 
 object Show {
-
   def show[A](value: A)(implicit s: Showable[A]): String = s.format(value)
-
-  def print[A](value: A)(implicit s: Showable[A]): Unit = println(s.format(value))
-
-  def format[A]: Showable[A] = {
-    //TODO replace this by a macro to find the fields names for any type A
-    new Showable[A] { 
-      override def format(a: A) = a match {
-        case Person(name, age) => s"Person(name = Name(firstName=${name.firstName}, lastName=${name.lastName}), age = $age)"   
-      }
-    }
-  }
-
 }
 
 object ShowableSyntax {
-  implicit class ShowtOps[A](value: A) {
 
-    def show(implicit s: Showable[A]): String = s.format(value)
-
-    def print(implicit s: Showable[A]): Unit = println(s.format(value))
-
+  implicit val stringShowable = new Showable[String] {
+    override def format(value: String) = value
   }
+
+  implicit val intShowable = new Showable[Int] {
+    override def format(value: Int) = value.toString
+  }
+
+  implicit class ShowtOps[A](value: A) {
+    def show(implicit s: Showable[A]): String = s.format(value)
+  }
+
 }
 
+
+
+@ShowMacro
+case class Name(firstName: String, lastName: String)
+
+@ShowMacro
+case class Person(name : Name, age: Int)
