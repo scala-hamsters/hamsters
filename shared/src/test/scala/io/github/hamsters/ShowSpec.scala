802313857ebc1f90f7283820ec6583699f04d85f
Raul Raja <raulraja@gmail.com>
2017-11-03 14:17:36 +0000

    Added test that demonstrates all instances are resolved implicitly

--- shared/src/test/scala/io/github/hamsters/ShowSpec.scala
+++ shared/src/test/scala/io/github/hamsters/ShowSpec.scala
@@ -1,6 +1,9 @@
 package io.github.hamsters
 
+import java.util.Date
+
 import org.scalatest.{FlatSpec, Matchers}
+
 class ShowSpec extends FlatSpec with Matchers {
 
   "Show on simple object" should "show field names and values of object" in {
@@ -11,6 +14,20 @@ class ShowSpec extends FlatSpec with Matchers {
   "Show on deeper object" should "show field names and values of object" in {
     val p = Person(Name("john", "doe"), 35)
     Show.show(p) should be("Person(name=Name(firstName=john,lastName=doe),age=35)")
+  }
+
+  "Show works on new instances as of #41" should "show field names and values of object" in {
+    """
+      |Show.show(AdditionalShowInstances(
+      |    '1',
+      |      1.toByte,
+      |      true,
+      |      1L,
+      |      1.0,
+      |      1f,
+      |      new Date(11111),
+      |      1.toShort))
+    """.stripMargin should compile
   }
 
 }
