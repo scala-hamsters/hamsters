85b4044315b13228d23be0674a91940e7fb6116d
Raul Raja <raulraja@gmail.com>
2017-11-03 13:39:00 +0000

    Added extra useful instances for `Show`

--- shared/src/main/scala/io/github/hamsters/Show.scala
+++ shared/src/main/scala/io/github/hamsters/Show.scala
@@ -1,5 +1,8 @@
 package io.github.hamsters
 
+import java.time.{LocalDate, LocalDateTime}
+import java.util.Date
+
 trait Showable[A] {
   def format(value: A): String
 }
@@ -16,6 +19,46 @@ object ShowableSyntax {
 
   implicit val intShowable = new Showable[Int] {
     override def format(value: Int) = value.toString
+  }
+
+  implicit val doubleShowable = new Showable[Double] {
+    override def format(value: Double) = value.toString
+  }
+
+  implicit val floatShowable = new Showable[Float] {
+    override def format(value: Float) = value.toString
+  }
+
+  implicit val longShowable = new Showable[Long] {
+    override def format(value: Long) = value.toString
+  }
+
+  implicit val jdkDateShowable = new Showable[Date] {
+    override def format(value: Date) = value.toString
+  }
+
+  implicit val jdkLocalDateShowable = new Showable[LocalDate] {
+    override def format(value: LocalDate) = value.toString
+  }
+
+  implicit val jdkLocalDateTimeShowable = new Showable[LocalDateTime] {
+    override def format(value: LocalDateTime) = value.toString
+  }
+
+  implicit val booleanShowable = new Showable[Boolean] {
+    override def format(value: Boolean) = value.toString
+  }
+
+  implicit val byteShowable = new Showable[Byte] {
+    override def format(value: Byte) = value.toString
+  }
+
+  implicit val shortShowable = new Showable[Short] {
+    override def format(value: Short) = value.toString
+  }
+
+  implicit val unitShowable = new Showable[Unit] {
+    override def format(value: Unit) = value.toString
   }
 
   implicit class ShowtOps[A](value: A) {
