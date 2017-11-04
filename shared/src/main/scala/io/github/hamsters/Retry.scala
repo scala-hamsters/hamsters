d94537e259b4581902e2177810565e8ce0441204
Loic Descotte <loic.descotte@gmail.com>
2017-11-04 09:11:57 +0100

    implement wait in retry

--- shared/src/main/scala/io/github/hamsters/Retry.scala
+++ shared/src/main/scala/io/github/hamsters/Retry.scala
@@ -1,6 +1,7 @@
 package io.github.hamsters
 
-import scala.util.{Try, Success, Failure}
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 object Retry {
  
@@ -16,17 +17,52 @@ object Retry {
     fromTry(maxTries, errorFn)(Try(fn))
   }
 
+  /**
+    * Retry a function several times
+    * @param maxTries number of retries
+    * @param errorFn the function to run if it fails, to handle the error message
+    * @param fn the function to run, returning a Try
+    * @tparam T
+    * @return Try of result
+    */
   def fromTry[T](maxTries: Int, errorFn: (String) => Unit = _=> Unit)(fn: => Try[T]): Try[T] = {
     @annotation.tailrec
-    def retry(nbTries: Int, errorFn: (String) => Unit)(fn: => Try[T]): Try[T] = {
+    def retry(remainingTries: Int, errorFn: (String) => Unit)(fn: => Try[T]): Try[T] = {
       fn match {
         case Success(x) => Success(x)
-        case _ if nbTries > 1 => retry(nbTries - 1, errorFn)(fn)
-        case Failure(e) =>
-          errorFn(s"Tried $maxTries times, still not enough : ${e.getMessage}")
-          Failure(e)
+        case _ if remainingTries > 1 => retry(remainingTries - 1, errorFn)(fn)
+        case Failure(failure) =>
+          errorFn(s"Tried $maxTries times, still not enough : ${failure.getMessage}")
+          Failure(failure)
       }
     }
-    retry(maxTries,errorFn)(fn)
+    retry(maxTries, errorFn)(fn)
   }
+
+  /**
+    * Retry a function several times
+    * @param maxTries number of retries
+    * @param waitInMilliSeconds number of milliseconds to wait before retrying
+    * @param errorFn the function to run if it fails, to handle the error message
+    * @param fn the function to run
+    * @tparam T
+    * @return Future result
+    */
+  def withWait[T](maxTries: Int, waitInMilliSeconds: Int, errorFn: (String) => Unit = _=> Unit)(fn: => T): Future[T] = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    def retry(remainingTries: Int, waitInMilliSeconds: Int, errorFn: (String) => Unit)(fn: => T): Future[T] = {
+      Future{
+        if(remainingTries < maxTries) Thread.sleep(waitInMilliSeconds)
+        fn
+      }.recoverWith{
+        case _ if remainingTries > 1 =>
+          retry(remainingTries - 1, waitInMilliSeconds, errorFn)(fn)
+        case failure =>
+          errorFn(s"Tried $maxTries times, still not enough : ${failure.getMessage}")
+          Future.failed(failure)
+      }
+    }
+    retry(maxTries, waitInMilliSeconds, errorFn)(fn)
+  }
+
 }
--- shared/src/test/scala/RetrySpec.scala
+++ shared/src/test/scala/RetrySpec.scala
@@ -1,10 +1,10 @@
 import io.github.hamsters.Retry
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{AsyncFlatSpec, Matchers}
 
 import scala.util.{Failure, Success, Try}
 import org.scalamock.scalatest.MockFactory
 
-class RetrySpec extends FlatSpec with Matchers with MockFactory {
+class RetrySpec extends AsyncFlatSpec with Matchers with MockFactory {
 
   val logErrorsFunctionMock = mockFunction[String, Unit]
 
@@ -44,4 +44,21 @@ class RetrySpec extends FlatSpec with Matchers with MockFactory {
     result shouldBe a [Failure[_]]
   }
 
+  "Retry" should "run function several times if failed with wait" in {
+    logErrorsFunctionMock expects "Tried 3 times, still not enough : failed"
+
+    val result = Retry.withWait(3, 3000, logErrorsFunctionMock) {
+      throw new Exception("failed")
+    }
+
+    result.failed. map { _ shouldBe a[Exception] }
+  }
+
+  "Retry" should "return result if no error with wait" in {
+      val result = Retry.withWait(3, 3000, logErrorsFunctionMock) {
+        1+1
+      }
+
+    result map { _ shouldBe 2 }
+  }
 }
