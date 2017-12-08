cd5abb5c83a23de553bfa0a9ab604977c7653eb2
Loïc Descotte <loic.descotte@gmail.com>
2017-12-08 11:50:57 +0100

    Update try-hamsters.sh

--- scripts/try-hamsters.sh
+++ scripts/try-hamsters.sh
@@ -4,7 +4,7 @@ test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P \
   com.lihaoyi:ammonite_2.12.3:1.0.1 \
-  io.github.scala-hamsters:hamsters_2.12:2.1.2 \
+  io.github.scala-hamsters:hamsters_2.12:2.2.1 \
   -- --predef-code 'import io.github.hamsters._;
                     import HList._;
                     import EmptyOptionValues._;
