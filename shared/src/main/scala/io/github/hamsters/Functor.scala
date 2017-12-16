c6ab4a70bb5fde849ce5d1cdb58d35db70fc4bfc
dgouyette <damien.gouyette@gmail.com>
2017-12-16 14:33:02 +0100

    mapN works for tuple2, tuple3 of Option[T], List[T]

--- shared/src/main/scala/io/github/hamsters/Functor.scala
+++ shared/src/main/scala/io/github/hamsters/Functor.scala
@@ -1,7 +1,20 @@
 package io.github.hamsters
 
+import scala.language.higherKinds
+
 trait Functor[Box[_]] {
 
-  def map[A, B](boxA: Box[A])(f: A => B): Box[B]
+  def map[In, Out](boxA: Box[In])(f: In => Out): Box[Out]
+
+}
+
+object Functor {
+  implicit val functorOption : Functor[Option] = new Functor[Option] {
+    override def map[In, Out](boxA: Option[In])(f: In => Out) = boxA.map(f)
+  }
+
+  implicit val functorList : Functor[List] = new Functor[List] {
+    override def map[In, Out](boxA: List[In])(f: In => Out) = boxA.map(f)
+  }
 
 }
---
+++ shared/src/main/scala/io/github/hamsters/Semigroup.scala
@@ -0,0 +1,74 @@
+package io.github.hamsters
+
+import scala.language.{higherKinds, implicitConversions}
+
+trait Semigroup[T] {
+  def combine(a: T, b: T): T
+}
+
+object Semigroup {
+
+  implicit val semigroupalInt: Semigroup[Int] = new Semigroup[Int] {
+    override def combine(a: Int, b: Int) = a + b
+  }
+
+  implicit val semigroupalString: Semigroup[String] = new Semigroup[String] {
+    override def combine(a: String, b: String) = s"$a$b"
+  }
+
+  implicit def semigroupalSeq[T]: Semigroup[Seq[T]] = new Semigroup[Seq[T]] {
+    override def combine(a: Seq[T], b: Seq[T]) = a ++ b
+  }
+
+  implicit def semigroupalList[T]: Semigroup[List[T]] = new Semigroup[List[T]] {
+    override def combine(a: List[T], b: List[T]) = a ++ b
+  }
+
+  implicit def semigroupalOption[T](implicit semigroup: Semigroup[T]): Semigroup[Option[T]] = new Semigroup[Option[T]] {
+    override def combine(maybeA: Option[T], maybeB: Option[T]) = {
+      (maybeA, maybeB) match {
+        case (Some(a), Some(b)) => Some(semigroup.combine(a, b))
+        case _ => None
+      }
+    }
+  }
+
+}
+
+trait Semigroupal[Box[_]] {
+  def product[A, B](a: Box[A], b: Box[B]): Box[(A, B)]
+}
+
+object Semigroupal {
+
+  implicit def semigroupalOption = new Semigroupal[Option] {
+    override def product[A, B](maybeA: Option[A], maybeB: Option[B]): Option[(A, B)] = (maybeA, maybeB) match {
+      case (Some(a), Some(b)) => Some(a -> b)
+      case _ => None
+    }
+  }
+
+  implicit def semigroupalList = new Semigroupal[List] {
+    override def product[A, B](listA: List[A], listB: List[B]): List[(A, B)] = for {
+      a <- listA
+      b <- listB
+    } yield a -> b
+  }
+}
+
+case class Tuple2Box[Box[_], T](boxA: Box[T], boxB: Box[T]) {
+  def mapN(f: (T, T) => T)(implicit functor : Functor[Box], s : Semigroupal[Box]): Box[T] = {
+    functor.map(s.product(boxA,boxB)){case (a,b)=> f(a,b)}
+  }
+}
+
+case class Tuple3Box[Box[_], T](boxA: Box[T], boxB: Box[T], boxC : Box[T]) {
+  def mapN(f: (T, T, T) => T)(implicit functor : Functor[Box], s : Semigroupal[Box]): Box[T] = {
+    functor.map(s.product(s.product(boxA,boxB), boxC)){case ((a, b), c) => f(a,b,c)}
+  }
+}
+
+object TupleBox {
+  implicit def t2x[Box[_],T](t: (Box[T], Box[T])) = Tuple2Box(t._1, t._2)
+  implicit def t3x[Box[_],T](t: (Box[T], Box[T], Box[T])) = Tuple3Box(t._1, t._2, t._3)
+}
